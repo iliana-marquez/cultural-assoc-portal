a6fe8eab944073807d622ca66d71f0b4488cc13a
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2026-06-08 22:24:44 +0200

    feat(routes): add GET verify route for PRG pattern

--- config/routes.php
+++ config/routes.php
@@ -19,5 +19,6 @@ $adminPath = $config['admin_path'];
 
 $router->get('/' . $adminPath,             'AuthController', 'showLogin');
 $router->post('/' . $adminPath,            'AuthController', 'sendOtp');
+$router->get('/' . $adminPath . '/verify', 'AuthController', 'showVerify');
 $router->post('/' . $adminPath . '/verify', 'AuthController', 'verifyOtp');
 $router->get('/logout',                    'AuthController', 'logout');
