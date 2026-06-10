e0d4e977aba59f9be1e9b84c6c014f90e7091936
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2026-06-11 01:06:12 +0200

    feat(controllers): add ContactController

---
+++ app/Controllers/ContactController.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * ContactController
+ * 
+ * Renders the contact page.
+ * Free intro section from pages table. 
+ * Contact information from organisation_info.
+ * Social links from urls table.
+ */
+
+require_once __DIR__ . '/BaseController.php';
+require_once __DIR__ . '/../Models/PagesModel.php';
+require_once __DIR__ . '/../Models/UrlModel.php';
+
+
+class ContactController extends BaseController
+{
+    public function index(array $params = []): void
+    {
+        $pagesModel = new PagesModel();
+        $urlModel = new UrlModel();
+
+        $sections = $pagesModel->getForPage('kontakt');
+        $urls = $urlModel->getForEntity('organisation', 1);
+
+        $this->render('pages/contact', [
+            'sections' => $sections,
+            'urls' => $urls,
+        ]);
+    }
+}
